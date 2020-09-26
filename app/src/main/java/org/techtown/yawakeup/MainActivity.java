36e425856cbdbfeeee07040bb5f90bbd873a0e67
bryankim <prunsoli@naver.cokm>
2020-09-26 20:27:40 +0900

    메뉴

--- app/src/main/java/org/techtown/yawakeup/MainActivity.java
+++ app/src/main/java/org/techtown/yawakeup/MainActivity.java
@@ -2,7 +2,9 @@ package org.techtown.yawakeup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onAlarm(View view){
+        Intent intent= new Intent(this,Alarm_Activity.class);
+        startActivity(intent);
+    }
+
+    public void onCheck(View view){
+        Intent intent= new Intent(this,Check_Activity.class);
+        startActivity(intent);
+    }
+    public void onMute(View view){
+        Intent intent= new Intent(this,Mute_Activity.class);
+        startActivity(intent);
+    }
+    public void onShare(View view){
+        Intent intent= new Intent(this,Share_Activity.class);
+        startActivity(intent);
+    }
+    public void onSet(View view){
+        Intent intent= new Intent(this,Set_Activity.class);
+        startActivity(intent);
+    }
+    public void onAcc(View view){
+        Intent intent= new Intent(this,Acc_Activity.class);
+        startActivity(intent);
     }
 }
