c0b530f82d567ffd0a65d66b12074eaf0b94537d
MSI <atomyoun@naver.com>
2020-10-10 01:28:14 +0900

    2.회원가입 + 디비 연동 파일

--- app/src/main/java/org/techtown/yawakeup/SignupActivity.java
+++ app/src/main/java/org/techtown/yawakeup/SignupActivity.java
@@ -1,102 +1,70 @@
 package org.techtown.yawakeup;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.HashMap;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.techtown.yawakeup.R;
+
 
 public class SignupActivity extends AppCompatActivity {
-
-    EditText et_signid,et_signpw;
-    Button btn_save;
-
-    private DatabaseReference mDatabase;
+    private FirebaseAuth firebaseAuth;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private EditText editTextName;
+    private Button buttonJoin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        et_signid = findViewById(R.id.et_signid);
-        et_signpw = findViewById(R.id.et_signpw);
-        btn_save = findViewById(R.id.btn_save);
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        //firebase 정의
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        editTextEmail = (EditText) findViewById(R.id.editText_email);
+        editTextPassword = (EditText) findViewById(R.id.editText_passWord);
+        editTextName = (EditText) findViewById(R.id.editText_name);
 
-        readUser();
-
-        btn_save.setOnClickListener(new View.OnClickListener() {
+        buttonJoin = (Button) findViewById(R.id.btn_join);
+        buttonJoin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String getUserId = et_signid.getText().toString();
-                String getUserPw = et_signpw.getText().toString();
-
-                //hashmap 만들기
-                HashMap result = new HashMap<>();
-                result.put("userId", getUserId);
-                result.put("userPw", getUserPw);
-
-                writeNewUser("1",getUserId,getUserPw);
-
-            }
-        });
-    }
-
-    private void writeNewUser(String userNum, String userId, String userPw) {
-        User user = new User(userId, userPw);
-
-        mDatabase.child("users").child(userNum).setValue(user)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        // Write was successful!
-                        Toast.makeText(SignupActivity.this, "저장을 완료했습니다.", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // Write failed
-                        Toast.makeText(SignupActivity.this, "저장을 실패했습니다.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-    }
-
-    private void readUser(){
-        mDatabase.child("users").child("1").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                // Get Post object and use the values to update the UI
-                if(dataSnapshot.getValue(User.class) != null){
-                    User post = dataSnapshot.getValue(User.class);
-                    Log.w("FireBaseData", "getData" + post.toString());
+                if (!editTextEmail.getText().toString().equals("") && !editTextPassword.getText().toString().equals("")) {
+                    // 이메일과 비밀번호가 공백이 아닌 경우
+                    createUser(editTextEmail.getText().toString(), editTextPassword.getText().toString(), editTextName.getText().toString());
                 } else {
-                    Toast.makeText(SignupActivity.this, "데이터 없음...", Toast.LENGTH_SHORT).show();
+                    // 이메일과 비밀번호가 공백인 경우
+                    Toast.makeText(SignupActivity.this, "계정과 비밀번호를 입력하세요.", Toast.LENGTH_LONG).show();
                 }
             }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                // Getting Post failed, log a message
-                Log.w("FireBaseData", "loadPost:onCancelled", databaseError.toException());
-            }
         });
+    }
+
+    private void createUser(String email, String password, String name) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 회원가입 성공시
+                            Toast.makeText(SignupActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            // 계정이 중복된 경우
+                            Toast.makeText(SignupActivity.this, "이미 존재하는 계정입니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
